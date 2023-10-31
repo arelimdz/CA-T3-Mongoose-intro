677abb97f19472bdf923f54ab2c2dec6ca6b04f0
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-31 21:03:30 +1100

    feat: create db schema and saved to the db

---
+++ src/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// Connect or create & connect to a database.
+
+async function databaseConnect(){
+    try{
+        // DB connection can take some time, eg. if DB is in the cloud 
+        await mongoose.connect('mongodb://localhost:27017/CatDB')
+        console.log("Database connected");
+
+    } catch (error){
+        console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
+	}
+}
+
+module.exports = {
+	databaseConnect
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //  This file handles the boot-up of the server
 
-
 const { app } = require('./server');
 
 app.listen(3000, () =>{
