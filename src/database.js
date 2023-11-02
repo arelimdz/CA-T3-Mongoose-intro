c4402161eab958a05ce7824be1dae10e04aeed7f
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-11-02 20:38:36 +1100

    feat: cat controller implemented

--- src/database.js
+++ src/database.js
@@ -3,15 +3,26 @@ const mongoose = require('mongoose');
 // Connect or create & connect to a database.
 
 async function databaseConnect(){
-    try{
-        // DB connection can take some time, eg. if DB is in the cloud 
-        await mongoose.connect('mongodb://localhost:27017/CatDB')
-        console.log("Database connected");
-
-    } catch (error){
-        console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
+	try {
+		// DB connection can take some time, eg. if DB is in the cloud 
+		console.log("Connecting to:\n" + process.env.DB_URI_LOCAL);
+		await mongoose.connect(process.env.DB_URI_LOCAL);
+		console.log("Database connected");
+	} catch (error) {
+		console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
 	}
 }
+
+// async function databaseConnect(){
+//     try{
+//         // DB connection can take some time, eg. if DB is in the cloud 
+//         await mongoose.connect('mongodb://localhost:27017/CatDB')
+//         console.log("Database connected");
+
+//     } catch (error){
+//         console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
+// 	}
+// }
 
 module.exports = {
 	databaseConnect
--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
+// Server configuration happens in server.js
+
 
 const express = require('express');
 
-// make a sertver instance
+// make a server instance 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.json({
-        messafe: "Hello world"
-    });
+app.get("/", (request, response) => {
+	response.json({
+		message:"Hello world"
+	});
 });
+
+const CatRouter = require('./controllers/CatController');
+app.use('/cats', CatRouter);
 
 module.exports = {
 	app
