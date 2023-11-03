26f12e288702c84f24a5a74eeb7ee4bca1260811
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-11-04 10:40:22 +1100

    feat: connect to cloud db

--- src/database.js
+++ src/database.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
 	try {
 		// DB connection can take some time, eg. if DB is in the cloud 
-		console.log("Connecting to:\n" + process.env.DB_URI_LOCAL);
-		await mongoose.connect(process.env.DB_URI_LOCAL);
+		console.log("Connecting to:\n" + process.env.DB_URI);
+		await mongoose.connect(process.env.DB_URI);
 		console.log("Database connected");
 	} catch (error) {
 		console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
